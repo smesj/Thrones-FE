d6f1e0ed560a1ee4850f2227a8ce62312bbc4b46
Sam Mesch <smeschishnick@gmail.com>
2019-08-13 15:07:59 -0600

    api urls

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -11,7 +11,7 @@ import Grid from '@material-ui/core/Grid';
 import axios from 'axios';
 
 const getPlayersFactions = (setPlayers) => {
-	axios.get('http://api.lastdaysofcheese.com/players/withFactions')
+	axios.get('https://api.lastdaysofcheese.com/players/withFactions')
 		.then((response) => {
 			setPlayers(response.data)
 		}).catch ((error) => {
