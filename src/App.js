f5129ad9e7636f882244d753e8b01a48c1ee3601
Sam Mesch <smeschishnick@gmail.com>
2019-08-15 14:05:42 -0600

    added auth to app

--- src/App.js
+++ src/App.js
@@ -1,112 +1,154 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { NavLink, Route, BrowserRouter } from 'react-router-dom'
+import { NavLink, Route, BrowserRouter, withRouter } from 'react-router-dom'
 import Scoreboard from './containers/Scoreboard';
 import Admin from './containers/Admin'
 import ScoreboardIcon from '@material-ui/icons/Dashboard';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import Hidden from '@material-ui/core/Hidden';
+import auth0Client from './Auth';
+import SecuredRoute from './securedRoute/SecuredRoute';
 
 // eslint-disable-next-line react/display-name
 const NavRef = React.forwardRef((props, ref) => <div ref={ref}><NavLink {...props}/></div>);
 
-function App() {
+function App(props) {
 
 	const classes = useStyles();
 
-	const [mobileOpen, setMobileOpen] = React.useState(false);
+	const [mobileOpen, setMobileOpen] = useState(false);
+	const [user, setUser] = useState(undefined);
+	const [checkingSession, setCheckingSession] = useState(true);
 
 	function handleDrawerToggle() {
 		setMobileOpen(!mobileOpen);
 	}
 
+	useEffect(() => {
+		const silentAuth = async () => {
+			if (props.location.pathname === '/callback') {
+				try {
+					await auth0Client.handleAuthentication();
+				} catch (err) {
+					console.log(err.error);
+				}
+			}
+
+			try {
+				await auth0Client.silentAuth();
+				setUser(auth0Client.getProfile());
+				props.history.replace('/');			
+			} catch (err) {
+				if (err.error !== 'login_required') console.log(err.error);
+			}
+		};
+		silentAuth()	
+	}, []);
+
+	const signOut = () => {
+		auth0Client.signOut();
+		setUser(auth0Client.getProfile());
+	};
+
 	return (
 		<div className={classes.app}>
-			<BrowserRouter>
-				<AppBar position="fixed" className={classes.appBar}>
-					<Toolbar>
-						<IconButton
-							color="inherit"
-							aria-label="open drawer"
-							edge="start"
-							onClick={handleDrawerToggle}
-							className={classes.menuButton}
-						>
-							<MenuIcon />
-						</IconButton>
-					<img src={process.env.PUBLIC_URL + 'assets/throne.PNG'} style={{width:50, paddingRight:8}} alt='throne'/>
-					<Typography variant="h6" noWrap>
-						Thrones 2019
-					</Typography>
-					</Toolbar>
-				</AppBar>
-				<Hidden smUp implementation="css">
-					<Drawer 
-						variant='temporary'
-						open={mobileOpen}
-						onClose={handleDrawerToggle}
-						anchor='left'
-						className={classes.drawer}
-						classes={{
-							paper: classes.drawerPaper,
-						}}
-						ModalProps={{
-							keepMounted: true, // Better open performance on mobile.
-						}}
+			<AppBar position="fixed" className={classes.appBar}>
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						edge="start"
+						onClick={handleDrawerToggle}
+						className={classes.menuButton}
 					>
-						{/* <div className={classes.toolbar} /> */}
-						<List>
-							<ListItem button component={NavRef} to='/'>
-								<ListItemIcon><ScoreboardIcon/></ListItemIcon>
-								<ListItemText>Scoreboard</ListItemText>
-							</ListItem>
-							<ListItem button component={NavRef} to='/admin'>
-								<ListItemIcon><ScoreboardIcon/></ListItemIcon>
-								<ListItemText>Admin</ListItemText>
-							</ListItem>
-						</List>
-					</Drawer>	
-				</Hidden>	
-				<Hidden xsDown implementation="css">
-					<Drawer 
-						variant='permanent'
-						anchor='left'
-						className={classes.drawer}
-						classes={{
-							paper: classes.drawerPaper,
-						}}
-					>
-						<div className={classes.toolbar} />
-						<List>
-							<ListItem button component={NavRef} to='/'>
-								<ListItemIcon><ScoreboardIcon/></ListItemIcon>
-								<ListItemText>Scoreboard</ListItemText>
-							</ListItem>
-							<ListItem button component={NavRef} to='/admin'>
-								<ListItemIcon><ScoreboardIcon/></ListItemIcon>
-								<ListItemText>Admin</ListItemText>
-							</ListItem>
-						</List>
-					</Drawer>
-				</Hidden>	
-				
-				<div className={classes.content}>
+						<MenuIcon />
+					</IconButton>
+				{/* <img src={process.env.PUBLIC_URL + 'assets/throne.PNG'} style={{width:50, paddingRight:8}} alt='throne'/> */}
+				<img src={'https://firebasestorage.googleapis.com/v0/b/thrones-818e8.appspot.com/o/throne.PNG?alt=media&token=bcc76194-ac99-4334-a200-dcaa5376f10a'} style={{width:50, paddingRight:8}} alt='throne'/>
+				<Typography variant="h6" noWrap>
+					Thrones 2019
+				</Typography>
+				</Toolbar>
+			</AppBar>
+			<Hidden smUp implementation="css">
+				<Drawer 
+					variant='temporary'
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					anchor='left'
+					className={classes.drawer}
+					classes={{
+						paper: classes.drawerPaper,
+					}}
+					ModalProps={{
+						keepMounted: true, // Better open performance on mobile.
+					}}
+				>
 					<div className={classes.toolbar} />
-					<Route path="/" exact component={Scoreboard} />
-					<Route path="/admin" exact component={Admin} />
-				</div>
-			</BrowserRouter>
+					<List>
+						<ListItem button component={NavRef} to='/'>
+							<ListItemIcon><ScoreboardIcon/></ListItemIcon>
+							<ListItemText>Scoreboard</ListItemText>
+						</ListItem>
+						<ListItem button component={NavRef} to='/admin'>
+							<ListItemIcon><ScoreboardIcon/></ListItemIcon>
+							<ListItemText>Admin</ListItemText>
+						</ListItem>
+					</List>
+				</Drawer>	
+			</Hidden>	
+			<Hidden xsDown implementation="css">
+				<Drawer 
+					variant='permanent'
+					anchor='left'
+					className={classes.drawer}
+					classes={{
+						paper: classes.drawerPaper,
+					}}
+				>
+					<div className={classes.toolbar} />
+					<List>
+						<ListItem button component={NavRef} to='/'>
+							<ListItemIcon><ScoreboardIcon/></ListItemIcon>
+							<ListItemText>Scoreboard</ListItemText>
+						</ListItem>
+						<ListItem button component={NavRef} to='/admin'>
+							<ListItemIcon><ScoreboardIcon/></ListItemIcon>
+							<ListItemText>Admin</ListItemText>
+						</ListItem>
+						{ !auth0Client.isAuthenticated() && 
+							<ListItem button onClick={auth0Client.signIn}>
+								<ListItemIcon><ScoreboardIcon/></ListItemIcon>
+								<ListItemText>Login</ListItemText>
+							</ListItem>
+						}
+						{ auth0Client.isAuthenticated() && 
+							<ListItem button onClick={() => {signOut()}}>
+								<ListItemIcon><ScoreboardIcon/></ListItemIcon>
+								<ListItemText>Logout + {auth0Client.getProfile().name}</ListItemText>
+							</ListItem>
+						}
+						
+					</List>
+				</Drawer>
+			</Hidden>	
+			
+			<div className={classes.content}>
+				<div className={classes.toolbar} />
+				<Route path="/" exact component={Scoreboard} />
+				<SecuredRoute path="/admin" exact component={Admin} />
+			</div>
 		</div>
 	);
 }
 
-export default App;
+export default withRouter(App);
 
 const drawerWidth = 240;
 
